e428b0b8896ab7a5e877c08f7129b3076c8d8fb7
Akila Sandakelum <asandakelum@virtusapolaris.com>
2017-03-03 17:39:28 +0530

    Adding ribon load balancer

---
+++ eureka-noun-ribbon-1/src/main/java/com/virtusa/EurekaNounApplication.java
@@ -0,0 +1,14 @@
+package com.virtusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaNounApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNounApplication.class, args);
+	}
+}
